b66b8412a16ebbecf93209d98cc6ea9d5fe52762
Jeff Moreland <jeff@evose.com>
2015-08-15 15:12:56 -0400

    PHP 5.4.1 compatible on login

--- login.php
+++ login.php
@@ -38,7 +38,8 @@
       extract($this_user,EXTR_PREFIX_ALL,"this");
     // Check encrypted password by seeding crypt with original pass (ref 1)
       
-      if (hash_equals($this_user_password, crypt($password, $this_user_password))) { // Password ok so continue
+    //if (hash_equals($this_user_password, crypt($password, $this_user_password))) { // Password ok so continue
+      if(crypt($password,$this_UserPassword) == $this_UserPassword) {
 
       // Set session id to unique value to prevent piggy-backing
         $id = uniqid("");
