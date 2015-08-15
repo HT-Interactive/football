9fc966bd3a33d342d7714666d9d18dee5980c288
Jeff Moreland <jeff@evose.com>
2015-08-15 14:42:30 -0400

    PostgreSQL script fix
    One script for development and local

--- mysql.php
+++ mysql.php
@@ -7,6 +7,9 @@
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
-  $db_nfl = pg_connect("host=173.254.28.69 dbname=evosecom_nfldb user=evosecom_nfl password=jmev0203")
-    or die('Could not connect: ' . pg_last_error());
+  $db_nfl = pg_connect("host=173.254.28.69 dbname=evosecom_nfldb user=evosecom_nfl password=jmev0203");
+  if(!$db_nfl) { //must be on evose, so try localhost
+    $db_nfl = pg_connect("host=localhost dbname=evosecom_nfldb user=evosecom_nfl password=jmev0203")
+      or die('Could not connect: ' . pg_last_error()); //something is wrong
+  }
 ?>
