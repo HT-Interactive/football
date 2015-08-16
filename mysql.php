3e062c28e43a7b7ee12a7739c3aa4d384ebaa594
Jeff Moreland <jeff@evose.com>
2015-08-15 21:15:24 -0400

    Local remote server error

--- mysql.php
+++ mysql.php
@@ -1,14 +1,19 @@
 <?php
 
-// Connect to DB
+// Connect to MySQL DB
   $db=mysqli_connect("localhost", "evosecom_nfl", "jmev0203","evosecom_wff");
   // Check connection
   if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
-  if(!$db_nfl = pg_connect("host=173.254.28.69 dbname=evosecom_nfldb user=evosecom_nfl password=jmev0203")) { //must be on evose, so try localhost
+// Connect to PostgreSQL DB
+  if($_SERVER['HTTP_HOST']=="evose.com") { //on evose server
     $db_nfl = pg_connect("host=localhost dbname=evosecom_nfldb user=evosecom_nfl password=jmev0203")
-      or die('Could not connect: ' . pg_last_error()); //something is wrong
+              or die('Could not connect: ' . pg_last_error()); //something is wrong
+  } else { //on local development
+    $db_nfl = pg_connect("host=173.254.28.69 dbname=evosecom_nfldb user=evosecom_nfl password=jmev0203")
+              or die('Could not connect: ' . pg_last_error()); //something is wrong
   }
+
 ?>
